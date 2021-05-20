b18cac844c6370f097c051f90836263418d48db5
Tom Erickson <tommytoyou@gmail.com>
2021-05-20 17:28:01 -0500

    page styling

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -14,7 +14,8 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    RouterModule
   ],
   declarations: [Tab2Page]
 })
